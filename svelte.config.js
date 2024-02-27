ca60bff40d67f61ae06dad5a24ae7c30df009f11
Dede <111243030+dedetheprogrammer@users.noreply.github.com>
2024-02-27 17:54:14 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,22 +13,21 @@ const config = {
 	// for more information about preprocessors
 	extensions: [ '.svelte', '.md' ],
 	preprocess: [ vitePreprocess(), mdsvex(mdsvexOptions) ],
-	paths: {
-		assets: 'https://dedetheprogrammer.github.io/dedetheprogrammer.github.io',
-		base: '' | '/dedetheprogrammer.github.io',
-	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(
-			{
-				pages: 'build',
-				assets: 'build',
-				strict: false,
-				fallback: '404.html'
-			}
-		)
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			strict: false,
+			fallback: '404.html'
+		}).
+		appDir: 'appDir',
+		paths: {
+			assets: 'https://dedetheprogrammer.github.io/svelte-test',
+			base: '/svelte-test',
+		}
 	}
 };
 
