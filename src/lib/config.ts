18f00c001bdcc67f1cf682f1be3f256823a70286
Dede <111243030+dedetheprogrammer@users.noreply.github.com>
2024-09-25 19:13:47 +0200

    hola

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from "$app/environment"
-
-export const title = "El rincón de Dede"
-export const description = "Me cago en todo su puto pecho"
-export const url = dev ? "http://localhost:5173/" : "url"
--- src/mdsvex.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts" context="module">
-	import { img } from '$lib/components/'
-	export { img }
-</script>
-
-<slot />
