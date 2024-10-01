061281542e17f06647539aab82d7c5a5ad1cbd4b
Dede <111243030+dedetheprogrammer@users.noreply.github.com>
2024-10-01 02:00:33 +0200

    Header mas bonito.

--- index.js
+++ index.js
@@ -12,8 +12,45 @@ const entries  = [
     document.getElementById("index-2")
 ];
 
+const tips = [
+    "Chief is working hard on it. Patience.",
+    "Did you know that Chief is studying computer engineering?",
+    "Of course you don't.",
+    "Error 404... Ups, not now.",
+    "Welcome.",
+    "Welcome back again.",
+    "Uhmm, now you really know... Some kind of paradox?",
+    "Nothing has changed the last time you visited me.",
+    "Chief, what are you doing?!",
+    "\"It always ends like this.\"",
+    "Did you know that Chief can also make videogames?",
+    "\"I never quite realized ... how beautiful this world is.\"",
+    "...",
+    "Chief is still working hard on it.",
+    "C'mon, don't be impatience, god.",
+    "Is Chief coming back one day..?",
+    "Where are you chief..?",
+    "Probably you have seen this loading screen for two years.",
+    "La vida es una lenteja o la tomas o la dejas",
+    "Chief is not working hard on this, don't be fooled.",
+    "Yes, Chief has come back, but still not working on this >:(",
+    "Chief is currently in the 4th year (finally T_T).",
+    "You can press Ctrl+Shift+º and see... uh oh...",
+    "I didn't suppose to tell you that... Fine whatever.",
+    "It doesn't work on mobile, jaja",
+    "The Ctrl+Shift+º was an idea I had, not working for now.",
+    "Don't listen to the others, I'm really working on this (for now).",
+    "*Upcoming categories... Animation?",
+    "PLAY OUTERWILDS RIGHT NOW!"
+]
+const tip = document.getElementById("tip");
+tip.textContent = tips[Math.floor(Math.random() * tips.length)];
+setInterval(() => {
+    tip.textContent = tips[Math.floor(Math.random() * tips.length)];
+}, 10000)
+
 // Escucha el evento de scroll
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', () => {
     // Si el usuario se desplaza más de 100px, muestra el botón
     if (window.scrollY > 100) {
         buttonUp.setAttribute("style", "right: 15px");
@@ -61,7 +98,7 @@ window.addEventListener('scroll', function() {
 });
 
 // Añade funcionalidad al botón para que al hacer click, vuelva al inicio
-buttonUp.addEventListener("click", function() {
+buttonUp.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth" // Scroll suave al inicio
